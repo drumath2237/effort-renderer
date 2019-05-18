6f5d73e6525b1f3a9f915faf94e3b0cccccfcfa9
drumath <2237k.drumath@gmail.com>
2019-05-18 22:17:48 +0900

    Rayのコードバァーって書いた

--- efffort-renderer/Ray.h
+++ efffort-renderer/Ray.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "Vector3.h"
+
+class Ray {
+public:
+	Vector3 origin;
+	Vector3 direction;
+
+	Ray() :origin(Vector3()), direction(Vector3(1,0,0)) {};
+	Ray(Vector3 o, Vector3 d) :origin(o), direction(normalize(d)) {};
+
+	Vector3 operator()(double t) const {
+		return origin + direction * t;
+	}
+};
