534952e73ca7765ffd82f40ad7a0f1218b129a4e
drumath <2237k.drumath@gmail.com>
2019-05-22 10:19:31 +0900

    setPixel,getPixelを実装

--- efffort-renderer/Image.h
+++ efffort-renderer/Image.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -8,13 +9,27 @@ class Image {
 public:
 
 	int width, height;
+	vector<double> data;
 
-	Image() :width(640), height(400) {};
-	Image(int w, int h) : width(w), height(h) {};
+	Image() :width(640), height(400) {
+		data = vector<double>(width*height, 0);
+	};
 
-	double getPixel(int x, int y);
-	void setPixel(int x, int y);
+	Image(int w, int h) : width(w), height(h) {
+		data = vector<double>(w * h, 0);
+	};
+
+	double getPixel(int x, int y)const;
+	void setPixel(int x, int y, double value);
 	void ppm_out(string filename);
-	Image gammma_correction();
+	Image gammma_correction()const;
 
 };
+
+double Image::getPixel(int x, int y) const {
+	return data[x * width + y];
+}
+
+void Image::setPixel(int x, int y, double value) {
+	data[x * width + y] = value;
+}
